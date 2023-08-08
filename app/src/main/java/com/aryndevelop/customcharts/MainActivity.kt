de7f21801635cdb53d42f4682b91695dd718f3f0
Shoaib <shoaib.shaikh@hiddenbrains.in>
2023-08-08 15:58:50 +0530

    Initial commit

---
+++ app/src/main/java/com/aryndevelop/customcharts/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.aryndevelop.customcharts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
